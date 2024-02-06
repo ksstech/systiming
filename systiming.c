101d72397ecbe6247eff7c4231e6d8b83cd7a875
André Maree <ammaree@users.noreply.github.com>
2024-02-06 22:59:54 +0200

    Update copyright year

--- systiming.c
+++ systiming.c
@@ -1,5 +1,5 @@
 /*
- * systiming.c - Copyright (c) 2014-23 Andre M. Maree / KSS Technologies (Pty) Ltd.
+ * systiming.c - Copyright (c) 2014-24 Andre M. Maree / KSS Technologies (Pty) Ltd.
  */
 
 #include <string.h>
--- systiming.h
+++ systiming.h
@@ -1,5 +1,5 @@
 /*
- * systiming.h - Copyright (c) 2014-23 Andre M. Maree / KSS Technologies (Pty) Ltd.
+ * systiming.h - Copyright (c) 2014-24 Andre M. Maree / KSS Technologies (Pty) Ltd.
  */
 
 #pragma once
